28e34fb68c85422abcc43674c3d30ab14b011029
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-09-25 10:00:05 -0300

    telefone

---
+++ telefone.py
@@ -0,0 +1,8 @@
+#lista telefonica
+nomes=["maria","luiz","eduardo","alex","thiago"]
+numeros=["(63)98467-2354","(63)98468-6808","(63)98448-3785","(63)98467-5432","(63)98493-4579"]
+#loop de repetição
+for i in range(len(nomes)):
+    nome =nomes[i]
+    numero =numeros[i]
+    print(f"nome: {nome}, numero: {numero}")
