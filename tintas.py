e92406207228b08a53d1358403c95e2f21279475
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-09-25 10:10:11 -0300

    tintas

---
+++ tintas.py
@@ -0,0 +1,7 @@
+metros=int(input("digite metros"))
+litros = metros/3
+valor=80
+capacidade= 18
+latas= litros/capacidade
+total= latas * valor
+print(f"voce precisara de {latas} latas o valor sera de {total} reais")
