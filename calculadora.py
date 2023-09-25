0626bdba89046d4661a4dfcc060fb03c9af54ab1
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-09-25 09:58:26 -0300

    calculadora

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+N1=int(input("digite um numero"))
+N2=input("digite uma operação")
+N3=int(input("digite um numero"))
+if N2 == "+":
+    print(N1+N3)
+if N2 == "-":
+    print(N1-N3)
+if N2 == "*":
+    print(N1*N3)
+if N2 == "/":
+    print(N1/N3)
