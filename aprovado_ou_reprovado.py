e14c95085ef0a93fdba8e98dfa7b303d9cd33bdd
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-09-25 09:58:12 -0300

    aprovado ou reprovado

---
+++ aprovado_ou_reprovado.py
@@ -0,0 +1,11 @@
+n1=int(input("digite um numero"))
+n2=int(input("digite um numero"))
+n3=int(input("digite um numero"))
+n4=int(input("digite um numero"))
+media = 0
+media = n1+n2+n3+n4
+print(media/4)
+if media/4 >= 7:
+    print("aprovado")
+else:
+    print("reprovado")
