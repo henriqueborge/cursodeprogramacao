1c626e11aa980e3aeb5e112956ecec4739feca7f
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-09-27 17:09:29 -0300

    velocidade media

---
+++ velocidade.py
@@ -0,0 +1,6 @@
+#definição de variaveis
+distancia=float(input("digite a distancia: "))
+tempo=float(input("digite tempo: "))
+#calculo de velociade media
+velocidade=distancia/(tempo/60)
+print(velocidade)
