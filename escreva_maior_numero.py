d906c44fb3ceafad84649dbd97647135eec2d1a6
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-09-28 15:21:01 -0300

    escreva o maior numero

---
+++ escreva_maior_numero.py
@@ -0,0 +1,7 @@
+numero1=int(input("digite primeiro numero:"))
+numero2=int(input("digite segundo numero:"))
+numero3=int(input("digite terceiro numero:"))
+numero4=int(input("digite quarto numero:"))
+numero5=int(input("digite quinto numero:"))
+lista= [numero1,numero2,numero3,numero4,numero5]
+print(max(lista))
