d51948a69b700c58dd63c3bb8521df95b5527dd7
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-09-27 21:24:43 -0300

    população

---
+++ populaçao.py
@@ -0,0 +1,10 @@
+paisA = 80000
+paisB = 200000
+tempo = 0
+while paisB > paisA:
+    populacaoA = paisA * 0.03
+    populacaoB = paisB * 0.015
+    paisA = populacaoA + paisA
+    paisB = populacaoB + paisB
+    tempo = tempo + 1
+    print("o pais A vai ultrapassar o pais B em",tempo,"anos")
