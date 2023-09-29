e2cbfa382a07ac14a4e241560626b94ca0207a4d
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-09-29 00:28:46 -0300

    carro

---
+++ carro.py
@@ -0,0 +1,5 @@
+dinheiro=float(input("digite o valor que você tem: "))
+valor = 5
+litro = dinheiro/valor
+km=litro*20
+print(f"Você podera abastecer {litro} litros e vai andar o equivalente a {km} quilometros.")
