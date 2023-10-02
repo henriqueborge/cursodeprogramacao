206e15e0f9b1d3dc5d6dde85c158b7a7a5bd1fc2
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-10-02 17:00:53 -0300

    função

--- funçao1.py
+++ funçao1.py
@@ -16,7 +16,14 @@ def imprimir_clientes(clientes):
         print(f"Email: {cliente ['Email']}")
         print(f"Telefone: {cliente ['Telefone']}")
         print("************************************")
-
+def atualizar_clientes(clientes, indice, nome, email, telefone):
+    if indice >= 0 and indice < len(clientes):
+        clientes[indice]['Nome']= nome
+        clientes[indice]['Email']= email
+        clientes[indice]['Telefone']= telefone
+        print("Dados atualizados com sucesso")
+    else:
+        print("indice do cliente invalido")
 clientes = []
 
 #Menu da função
@@ -24,7 +31,8 @@ while True:
     print("MENU\n")
     print("1. Cadastrar cliente")
     print("2. Imprimir cliente")
-    print("3. Sair")
+    print("3. atualizar cliente")
+    print("4. Sair")
     
     opcao = int(input("Escolha uma opção:"))
     print("************************************")
@@ -37,6 +45,12 @@ while True:
     elif opcao == 2:
         imprimir_clientes(clientes)
     elif opcao == 3:
+        indice= int(input("digite indice do cliente: "))
+        nome= input("digite novo nome: ")
+        email= input("digite novo email: ")
+        telefone= input("digite novo telefone: ")
+        atualizar_clientes(clientes, indice, nome, email, telefone)
+    elif opcao == 4:
         print("saindo.")
         break
     else:
