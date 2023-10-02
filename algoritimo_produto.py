8515c06fa4fb4ba0464250d45afb1d2eeaf6b818
Luis Henrique de Sousa Borges Nascimento <144963731+henriqueborge@users.noreply.github.com>
2023-10-02 17:01:15 -0300

    algoritimo produto

--- algoritimo_produto.py
+++ algoritimo_produto.py
@@ -29,7 +29,22 @@ def imprimir_produtos(produtos):
         print(f"Custo: {produto ['Custo']}")
         print(f"venda: {produto ['venda']}")
         print(f"lucro: {produto ['lucro']}")
-        
+def atualizar_produto(produtos, indice, nome, valor, quantidade, imp1, imp2, imp3, frete, custo, lucro, venda):
+    if indice >= 0 and indice < len(produtos):
+        produtos[indice]['Nome']=nome
+        produtos[indice]['Valor']=valor
+        produtos[indice]['Quantidade']=quantidade
+        produtos[indice]['imposto1']=imp1
+        produtos[indice]['imposto2']=imp2
+        produtos[indice]['imposto3']=imp3
+        produtos[indice]['Frete']=frete
+        produtos[indice]['Custo']=custo
+        produtos[indice]['lucro']=lucro
+        produtos[indice]['venda']=venda
+        print("dados do produto atualizado com sucesso")
+    else:
+        print("indice do produto invalido")
+
 produtos = []
 
 #Menu da função
@@ -37,7 +52,8 @@ while True:
     print("MENU\n")
     print("1. cadastrar produto")
     print("2. imprimir produto")
-    print("3. sair")
+    print("3. atualizar produto")
+    print("4. sair")
     
     opcao = int(input("Escolha uma opção"))
     
@@ -63,6 +79,24 @@ while True:
     elif opcao == 2:
         imprimir_produtos(produtos)
     elif opcao == 3:
+        indice= int(input("digite indice do produto"))
+        nome= input("digite novo nome do produto")
+        valor = float(input("digite novo valor do produto: "))
+        quantidade = int(input("digite a nova quantidade: "))
+        imp1 = float(input("digite o primeiro imposto: "))
+        imp2 = float(input("digite o segundo imposto: "))
+        imp3 = float(input("digite o terceiro imposto: "))
+        frete = int(input("digite valor do frete: "))
+        lucro = float(input("digite quanto deseja de lucro: "))
+        imp1=((valor*imp1)/100)
+        imp2=((valor*imp2)/100)
+        imp3=((valor*imp3)/100)
+        valorfrete=frete/quantidade
+        custo = (valor+imp1+imp2+imp3+valorfrete)
+        lucro = ((custo*lucro)/100)
+        venda = custo+lucro
+        atualizar_produto(produtos, indice, nome, valor, quantidade, imp1, imp2, imp3, frete, custo, lucro, venda)
+    elif opcao == 4:
         print("saindo.")
         break
     else:
